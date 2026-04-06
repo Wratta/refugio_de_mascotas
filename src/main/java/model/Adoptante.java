5624538d40789a4fa7169b686f483c0968c7579e
Jorge Casasempere Sasso <wratta@gmail.com>
2026-04-06 14:34:17 +0200

    Añadido a model la clase Adoptante
    Añadido a model la clase Adoptante

---
+++ src/main/java/model/Adoptante.java
@@ -0,0 +1,21 @@
+package model;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Adoptante {
+    private String dni; // Identificador legal obligatorio
+    private String nombre;
+    private String apellidos;
+    private String telefono;
+    private String email;
+
+    // Requisitos Ley de Bienestar Animal
+    private boolean cursoCompetencias; // ¿Ha hecho el curso para perros?
+    private boolean antecedentesMaltrato; // Declaración responsable (debe ser false)
+    private boolean contratoFirmado;
+}
